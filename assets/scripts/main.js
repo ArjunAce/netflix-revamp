9806c429cd34aa333153a8a00c93302f9333cff3
Arjun M <arjun.mudhaliyar@gmail.com>
2019-02-03 15:58:25 +0530

    Animation started

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+function init(){
+    // alert('zxc');
+}
+
+function searchIconHandler(){
+    /*var iconSet = document.getElementsByClassName('toolIcon');
+    for (var i = 0; i < iconSet.length; i++) {
+        // iconSet[i].addC
+    }*/
+    $(".tools img").addClass("toolSet");
+    $(".toolInactive").css("color", "yellow");
+
+}
