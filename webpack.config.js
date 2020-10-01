203c961316bd364bac3c5f09599519f534b1324d
arjunace <arjun.mudhaliyar@gmail.com>
2020-10-02 00:07:23 +0530

    Added contentHash to output filename Added HtmlWebpackPlugin to include the output file with dynamic hashname

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main.[contentHash].js",
         path: path.resolve(__dirname, "dist")
     },
     module: {
@@ -18,5 +19,8 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html' })
+    ]
 }
