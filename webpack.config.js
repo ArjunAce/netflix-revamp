6a2eac016e7065b9345dc58f3117a45283519ab4
arjunace <arjun.mudhaliyar@gmail.com>
2020-10-02 14:33:46 +0530

    Added mini-css-extract-plugin to extract css to a separate file Added optimize-css-assets-webpack-plugin to minify this css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     mode: "development",
@@ -12,12 +15,19 @@ module.exports = {
         filename: "[name].[contentHash].bundle.js",
         path: path.resolve(__dirname, "dist")
     },
+    optimization: {
+        minimizer: [
+            new TerserJSPlugin(),
+            new OptimizeCSSAssetsPlugin()
+        ],
+    },
     module: {
         rules: [
             {
                 test: /\.scss$/,
                 use: [
-                    "style-loader", // 3. Inject styles into DOM
+                    // "style-loader", // 3. Inject styles into DOM
+                    MiniCssExtractPlugin.loader,
                     "css-loader",   // 2. Turns css into commonjs
                     "sass-loader" // 1. Turns scss to css
                 ]
@@ -40,6 +50,7 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({ template: './src/index.html' })
+        new HtmlWebpackPlugin({ template: './src/index.html' }),
+        new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" })
     ]
 }
