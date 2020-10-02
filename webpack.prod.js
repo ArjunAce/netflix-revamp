b18bf28fe8cbd230d82ad047d210b9bc89bce4cc
arjunace <arjun.mudhaliyar@gmail.com>
2020-10-02 15:42:25 +0530

    Split webpack config into common, dev and prod config.

---
+++ webpack.prod.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const commonConfig = require('./webpack.common');
+const { merge } = require('webpack-merge');
+
+module.exports = merge(commonConfig, {
+    mode: 'production',
+    output: {
+        filename: '[name].[contentHash].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCSSAssetsPlugin(),
+            new TerserJSPlugin(),
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: '[name].[contentHash].css' })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, // 3. Load extracted and minified CSS in head
+                    'css-loader',   // 2. Turns css into commonjs
+                    'sass-loader' // 1. Turns scss to css
+                ]
+            }
+        ]
+    },
+
+});
