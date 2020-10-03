809cb2bb3c6836caf478ee63964375cb57234ad2
arjunace <arjun.mudhaliyar@gmail.com>
2020-10-03 17:01:20 +0530

    Installed and configured react

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 import './styles/main.scss';
-console.log("Hello World!");
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
--- webpack.common.js
+++ webpack.common.js
@@ -10,6 +10,16 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
                 test: /\.html$/,
                 use: ['html-loader']
             },
